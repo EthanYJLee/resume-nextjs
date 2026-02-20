533b8907beeb496e79276aebe524cb2db9f92249
EthanYJLee <lyj72011648@gmail.com>
2026-02-20 18:44:34 +0900

    update contents

--- payload/en/language.ts
+++ payload/en/language.ts
@@ -7,13 +7,13 @@ const language: ILanguage.Payload = {
       title: 'English',
       date: '2025-08',
       certificate: 'TOEIC',
-      score: '890',
+      score: '890 (150724)',
     },
     {
       title: 'Japanese',
       date: '2025-08',
       certificate: 'JLPT',
-      score: 'N3',
+      score: 'N3 (N3A514230A)',
     },
   ],
 };
--- payload/language.ts
+++ payload/language.ts
@@ -20,13 +20,13 @@ const language: ILanguage.Payload = {
       title: '영어',
       date: '2025-08',
       certificate: 'TOEIC',
-      score: '890',
+      score: '890(150724)',
     },
     {
       title: '일본어',
       date: '2025-08',
       certificate: 'JLPT',
-      score: 'N3',
+      score: 'N3(N3A514230A)',
     },
   ],
 };
