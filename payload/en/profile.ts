5bebd8454d9b5bc7b30720f8cce97cf878a20737
Youngjin Lee <113097293+EthanYJLee@users.noreply.github.com>
2026-08-03 14:16:26 +0900

    Update profile image import path

--- payload/en/profile.ts
+++ payload/en/profile.ts
@@ -2,7 +2,8 @@ import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faBell } from '@fortawesome/free-regular-svg-icons';
 import { IProfile } from '../../component/profile/IProfile';
-import image from '../../asset/profile_image.jpg';
+// import image from '../../asset/profile_image.jpg';
+import image from '../asset/260803_profile.jpeg';
 
 const profile: IProfile.Payload = {
   disable: false,
